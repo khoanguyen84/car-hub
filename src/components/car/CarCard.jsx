14f91d9d8de3cc41981fcd2216b47569d6e25976
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-01-03 16:02:01 +0700

    update template

--- src/components/car/CarCard.jsx
+++ src/components/car/CarCard.jsx
@@ -1,32 +1,34 @@
 import gas from '../../../public/gas.svg'
 import tire from '../../../public/tire.svg'
 import steeringWheel from '../../../public/steering-wheel.svg'
-export default function CarCard() {
+export default function CarCard({ car }) {
     return (
         <div className="car-card">
-            <div className="car-card-header">Honda Odyssey</div>
+            <div className="car-card-header text-capitalize">{car?.make} {car?.model}</div>
             <div className="car-card-body">
                 <h5>
-                    <span style={{ position: 'relative', top: -20, fontSize: 14}}>$</span>
+                    <span style={{ position: 'relative', top: -20, fontSize: 14 }}>$</span>
                     <span className='fw-bolder fs-2'>52</span>
-                    <span style={{ position: 'relative', top: 5, fontSize: 14}}>/day</span>
+                    <span style={{ position: 'relative', top: 5, fontSize: 14 }}>/day</span>
                 </h5>
-                <img className="card-image" src="https://cdn.imagin.studio/getimage?customer=hrjavascript-mastery&make=chevrolet&modelFamily=corvette&zoomType=fullscreen&modelYear=2020" alt="" />
+                <img className="card-image" src={`https://cdn.imagin.studio/getimage?customer=hrjavascript-mastery&make=${car?.make}&modelFamily=${car?.model.split(' ')[0]}&zoomType=fullscreen&modelYear=${car?.year}`} alt="" />
             </div>
             <div className="car-card-footer">
                 <div>
                     <div className='d-flex align-items-center justify-content-between'>
                         <div className='d-flex flex-column align-items-center flex-grow-1'>
                             <img className='icon' src={steeringWheel} alt="" />
-                            <span className='pt-1'>Automation</span>
+                            <span className='pt-1'>
+                                {car?.transmission === "a" ? "Automatic" : "Manual"}
+                            </span>
                         </div>
                         <div className='d-flex flex-column align-items-center flex-grow-1'>
                             <img className='icon' src={tire} alt="" />
-                            <span className='pt-1'>FWD</span>
+                            <span className='pt-1'>{car?.drive.toUpperCase()}</span>
                         </div>
                         <div className='d-flex flex-column align-items-center flex-grow-1'>
                             <img className='icon' src={gas} alt="" />
-                            <span className='pt-1'>19 MPG</span>
+                            <span className='pt-1'>{car?.city_mpg} MPG</span>
                         </div>
                     </div>
                 </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import CarCard from "../components/car/CarCard";
 import MainLayout from "../layouts/MainLayout";
+import { manufacturers, yearsOfProduction, fuels } from '../constants'
 
 export default function HomePage() {
     const [carList, setCarList] = useState([])
     useEffect(() => {
         async function getCarList() {
-            let carListRes = await fetch('https://jsonserver-vercel-api.vercel.app/carList')
+            let carListRes = await fetch('https://jsonserver-vercel-api.vercel.app/carList?_page=1&_limit=10')
             let data = await carListRes.json()
             setCarList(data)
         }
@@ -24,28 +25,53 @@ export default function HomePage() {
                         <div className="col-lg-7 col-md-7 col-sm-12">
                             <div className="row">
                                 <div className="col-md-6">
-                                    <select className="form-select">
-                                        <option value="Honda Odyssey">Honda Odyssey</option>
-                                    </select>
+                                    <div className="form-group">
+                                        <label className="form-label">Manufacturers</label>
+                                        <select className="form-select" defaultValue={''}>
+                                            <option value={""}>Manufacturers</option>
+                                            {
+                                                manufacturers?.map((manu) => (
+                                                    <option key={manu} value={manu}>{manu}</option>
+                                                ))
+                                            }
+                                        </select>
+                                    </div>
                                 </div>
                                 <div className="col-md-6">
-                                    <select className="form-select">
-                                        <option value="Honda Odyssey">Honda Odyssey</option>
-                                    </select>
+                                    <div className="form-group">
+                                        <label className="form-label">Model</label>
+                                        <input className="form-control" placeholder="Enter car model..." />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-5 col-md-5 col-sm-12">
                             <div className="row">
                                 <div className="col-md-6">
-                                    <select className="form-select">
-                                        <option value="Honda Odyssey">Honda Odyssey</option>
-                                    </select>
+                                    <div className="form-group">
+                                        <label className="form-label">Fuels</label>
+                                        <select className="form-select" defaultValue={''}>
+                                            <option value={""}>Fuel</option>
+                                            {
+                                                fuels?.map((item) => (
+                                                    <option key={item.title} value={item.value}>{item.title}</option>
+                                                ))
+                                            }
+                                        </select>
+                                    </div>
                                 </div>
                                 <div className="col-md-6">
-                                    <select className="form-select">
-                                        <option value="Honda Odyssey">Honda Odyssey</option>
-                                    </select>
+                                    <div className="form-group">
+                                        <label className="form-label">Year</label>
+                                        <select className="form-select" defaultValue={'Year'}>
+                                            <option value={""}>Year</option>
+                                            {
+                                                yearsOfProduction?.map((item) => (
+                                                    <option key={item.title} value={item.value}>{item.title}</option>
+                                                ))
+                                            }
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -55,8 +81,8 @@ export default function HomePage() {
                     <div className="row">
                         {
                             carList?.map((car) => (
-                                <div className="col-lg-4 col-md-4 col-sm-6 mb-4">
-                                    <CarCard />
+                                <div className="col-lg-3 col-md-3 col-sm-6 mb-4">
+                                    <CarCard car={car} />
                                 </div>
                             ))
                         }
