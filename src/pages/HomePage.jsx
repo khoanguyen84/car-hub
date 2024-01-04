0e83dc73cc2367aac9c4738ae44df5eb2bdde402
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-01-04 15:02:58 +0700

    update template

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -81,7 +81,7 @@ export default function HomePage() {
                     <div className="row">
                         {
                             carList?.map((car) => (
-                                <div className="col-lg-3 col-md-3 col-sm-6 mb-3">
+                                <div className="col-lg-3 col-md-4 col-sm-6 mb-3">
                                     <CarCard car={car} />
                                 </div>
                             ))
